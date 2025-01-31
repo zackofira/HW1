a0c35eb3ec8a055e9ce9672cca6fce010a97f758
zackofira <150864953+zackofira@users.noreply.github.com>
2025-01-30 22:14:50 -0600

    Update HW1.java

--- HW1.java
+++ HW1.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION  HERE ***
+ * *** Zach Kofira | Section 002 ***
  *
  * Homework # 1 (Programming Assignment). This Java class defines some basic
  * manipulation operations on Linked-Lists and Stacks.
@@ -13,235 +13,211 @@
 
 import java.util.Stack;
 
-public class HW1 {
+public class Main {
+    public static void main(String[] args) {
+        int     assignmentScore = 0;
+        boolean errorFlag = false;
 
-    /*
-     * Class LinkedList
-     *
-     * This class builds a singly linked list. Each node of the linked-list
-     * contains a single integer values.
-     *
-     * Methods:
-     *  - void   sortInserted(val)     - Inserts value 'val' into the linked-list in
-     *                                   sorted fashion
-     *  - void   removeElementsLT(val) - Removed values in the linked-list that are less
-     *                                   than 'val'
-     *  - void   removeElement(val)    - Removes all values in the linked list of
-     *                                   value 'val'
-     *  - String toString()            - coverts and returns the linked-lists as a string
-     *                                   delimited by brackets []
-     *
-     */
+        System.out.println("\nProgram testing driver starting  ...");
 
-    static class LinkedList {
-        static class Node {
-            int data;
-            Node next;
+        /**************************************
+         *
+         * Testing of the LinkedList code.
+         *
+         ***************************************/
 
-            Node(int d)  {        // Constructor
-                data = d;
-                next = null;
-            }
+
+        HW1.LinkedList list1 = new HW1.LinkedList();
+
+        // Insert the values
+        list1.sortedInsert(1);
+        list1.sortedInsert(2);
+        list1.sortedInsert(3);
+        list1.sortedInsert(4);
+        list1.sortedInsert(1);
+        list1.sortedInsert(2);
+        list1.sortedInsert(3);
+        list1.sortedInsert(4);
+        list1.sortedInsert(9);
+        list1.sortedInsert(100);
+        list1.sortedInsert(35);
+
+        // System.out.println("Starting Linked-Lists testing ...");
+        // System.out.println("After initial insertions: " + list1);
+
+        if ( ! list1.toString().equals("[1 1 2 2 3 3 4 4 9 35 100]") ) {
+            System.out.println("ERROR 1: Linked-list test failed sorted insertions");
+            errorFlag = true;
         }
-        Node head;                // head of Linked-list
 
+
+        list1.removeElementsLT(8);  // remove items with a value less than 8
+        if ( ! errorFlag && ! list1.toString().equals("[9 35 100]") ) {
+            System.out.println("ERROR 2: Linked-list test failed removing elements LT");
+            errorFlag = true;
+        }
+
+
+        list1.removeElement(35);  // remove items with a value of 35
+        if ( ! errorFlag && ! list1.toString().equals("[9 100]") ) {
+            System.out.println("ERROR 3: Linked-list test failed removing elements equal");
+            errorFlag = true;
+        }
+
+        // Additional link-list tests
+        HW1.LinkedList list2 = new HW1.LinkedList();
+
+        list2.sortedInsert(1003);
+        list2.sortedInsert(4004);
+        list2.sortedInsert(05);
+        list2.sortedInsert(5);
+        list2.sortedInsert(59);
+        list2.sortedInsert(995);
+        list2.sortedInsert(5);
+
+        if ( ! errorFlag && ! list2.toString().equals("[5 5 5 59 995 1003 4004]") ) {
+            System.out.println("ERROR 4: Linked-list test failed sorted insertions");
+            errorFlag = true;
+        }
+
+        list2.removeElement(35);  // remove items with a value of 35
+        if ( ! errorFlag && ! list2.toString().equals("[5 5 5 59 995 1003 4004]") ) {
+            System.out.println("ERROR 5: Linked-list test failed removing elements equal");
+            errorFlag = true;
+        }
+
+        list2.removeElement(995);  // remove items with a value of 35
+        if ( ! errorFlag && ! list2.toString().equals("[5 5 5 59 1003 4004]") ) {
+            System.out.println("ERROR 6: Linked-list test failed removing elements equal");
+            errorFlag = true;
+        }
+
+
+        list2.removeElementsLT(35);  // remove items with a value of 35
+        if ( ! errorFlag && ! list2.toString().equals("[59 1003 4004]") ) {
+            System.out.println("ERROR 7: Linked-list test failed removing elements LT");
+            errorFlag = true;
+        }
 
         /*
-         * Method sortedInsert() - this method will insert a new node to the
-         * linked list containing the value specific in teh parameter 'data'.
-         * The newly inserted node will be inserted in sorted order within
-         * the linked-list.
-         *
+         * Score the Link-list portion (max points 25)
          */
-        public void sortedInsert ( int data ) {
-            Node new_node = new Node(data);
 
-            new_node.next = null;
-
-            // Special case for head node.
-            if (this.head == null || head.data >= new_node.data ) {
-                new_node.next = head;
-                head = new_node;
-            } else {
-                // locate the node before the point of insertion
-                Node current = this.head;
-
-                // Identify where to place the item to insert
-                while (current.next != null && current.next.data < data) {
-                    current = current.next;
-                }
-                new_node.next = current.next;
-                current.next = new_node;
-            }
-
-            return;
-        }
+        if ( ! errorFlag )
+            assignmentScore += 25;
+        else
+            errorFlag = false;
 
 
-        /*
-         * Method removeElementsLT() - this method removes all nodes that contain a
-         * value that is less than the provided parameter 'ltValue'.
+        /**************************************
          *
-         * The method will invoke the method removeElements for each element
-         * found in the linked-list that is less than thr parameter value passed.
-         */
-        public void removeElementsLT ( int ltValue ) {
+         * Testing of isPalindrone code.
+         *
+         ***************************************/
 
-            // YOUR CODE GOES HERE
+        // System.out.println("Starting isPalindrone testing ...");
 
-            return;
+        if ( ! HW1.Stacks.isPalindrome("Was it a car or a cat I saw")) {
+            System.out.println("ERROR 8: ispalindrome test failed");
+            errorFlag = true;
         }
 
+        if ( ! errorFlag && ! HW1.Stacks.isPalindrome("Race Car")) {
+            System.out.println("ERROR 9: ispalindrome test failed");
+            errorFlag = true;
+        }
+
+        if ( ! errorFlag && ! HW1.Stacks.isPalindrome("Never odd or even")) {
+            System.out.println("ERROR 10: ispalindrome test failed");
+            errorFlag = true;
+        }
+
+        if ( ! errorFlag && HW1.Stacks.isPalindrome("civic2")) {
+            System.out.println("ERROR 11: ispalindrome test failed");
+            errorFlag = true;
+        }
 
         /*
-         * Method removeElement() - this method removes all nodes that contain a
-         * value equal to the value the provided parameter 'value'.
+         * Score the isPalindrome portion (max points 25)
          */
 
-        public void removeElement ( int value ) {
+        if ( ! errorFlag )
+            assignmentScore += 25;
+        else
+            errorFlag = false;
 
-            // YOUR CODE GOES HERE
 
-            return;
+        /**************************************
+         *
+         * Testing of findLargestK code.
+         *
+         ***************************************/
+
+        // System.out.println("Starting findLargestK testing ...");
+
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(5);
+        myStack.push(3);
+        myStack.push(1);
+        myStack.push(4);
+        myStack.push(1);
+
+        // System.out.println(myStack);
+
+        if ( HW1.Stacks.findLargestK(myStack, 1) != 4) {
+            System.out.println("ERROR 12: findLargestk test failed");
+            errorFlag = true;
         }
 
+        if ( ! errorFlag && HW1.Stacks.findLargestK(myStack, 5) != 0) {
+            System.out.println("ERROR 13: findLargestk test failed");
+            errorFlag = true;
+        }
+
+        if ( ! errorFlag && HW1.Stacks.findLargestK(myStack, 4) != 3) {
+            System.out.println("ERROR 14: findLargestk test failed");
+            errorFlag = true;
+        }
 
         /*
-         * Method toString() - this is a helper method for printing / constructing
-         * a string object from the linked-list.
+         * Score the findLargestK portion (max points 26)
          */
-        public String toString () // Method to output the LinkedList as a String
-        {
-            String output = "[";
-            Node currNode = this.head;
-            while (currNode != null) {
-                output += currNode.data + " ";
-                currNode = currNode.next;
-            }
-            return output.trim() + "]";
+
+        if ( ! errorFlag )
+            assignmentScore += 26;
+        else
+            errorFlag = false;
+
+
+        /**************************************
+         *
+         * Testing of the algorithm analysis.
+         *
+         ***************************************/
+
+        System.out.println("\nThe algorithm analysis testing skipped (12 points each, total 24 points) ...");
+
+        if ( HW1.algorithmAnalysis1(5,5) != 3 ) {
+            System.out.println("ERROR 15: algorithmAnalysis1 test failed");
+            errorFlag = true;
         }
 
-    } // End class LinkedList
+        if ( ! errorFlag )
+            assignmentScore += 12;
+        else
+            errorFlag = false;
 
-
-
-
-    /*
-     * Class Stacks
-     *
-     * This class utilizes the Java Common Framework Library Stack class.
-     *
-     * The intent of this class is to write methods which utilize the Java
-     * library's Stack class. You need to utilize this library class in
-     * your solution.
-     *
-     * Methods:
-     *  - boolean isPalindrome(string)   - method returns true or false if 'string'
-     *                                     is a palindrome
-     *  - int     findlargestK(stack, k) - method accepts a stack and returns the
-     *                                     the largest index in the stack containing
-     *                                     value 'k' (stack index starts at 0)
-     *
-     */
-
-    static class Stacks {
-
-        /*
-         * Method isPalindrome() - This method will return the boolean value 'true'
-         * or 'false' on if the passed in parameter string is a palindrome or not.
-         *
-         * The routine should be case insensitive! Meaning 'RaCe cAr' is a palindrome.
-         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are plaindromes.
-         *
-         * The method should utilize the provided Stack class.
-         */
-        public static boolean isPalindrome(String input) {
-
-            Stack<Character> stack = new Stack<>();
-            input = input.toLowerCase().replaceAll("\\s+", "");
-
-            // Your CODE GOES HERE
-            return false;
+        if ( HW1.algorithmAnalysis2(2) != 2 ) {
+            System.out.println("ERROR 16: algorithmAnalysis1 test failed");
+            errorFlag = true;
         }
 
+        if ( ! errorFlag )
+            assignmentScore += 12;
+        else
+            errorFlag = false;
 
-        /*
-         * Method findLargestk() - This method will return the largest index
-         * position in the stack for the value specified by the parameter 'k'.
-         *
-         * Note that the bottom of the stack is index location 0. So it you push
-         * on to the stack the values 3 4 9 4 4 7 4, in that order. And you pass the
-         * value '4' for the parameter k, then the largest index position is index
-         * location 6.
-         *
-         * The method should utilize the provided Stack class. This method should NOT
-         * destroy the passed in stack, meaning when the method returns, the passed in
-         * stack should be identical to when this method is passed. One trick as you
-         * pop elements off the passed in stack, place them in a temp stack. Then when
-         * completed, place them all back in teh original stack.
-         */
-        public static int findLargestK(Stack<Integer> stack, int k) {
-
-            // YOUR CODE GOES HERE
-            return -1;
-        }
-
-    }  // End class Stacks
-
-
-
-    /*******************************
-     *
-     * Algorithm Analysis
-     *
-     * Below are two methods, algorithmAnalysis1 and algorithmAnalysis2.
-     * Modify the return statement to return the correct option number.
-     *
-     *********************************/
-
-    public static int algorithmAnalysis1(int n, int m) {
-        int a = 0, b = 0;
-
-        for (int i=0; i < n; i++)
-            a+= Math.random();
-
-        for (int j=0; j < m; j++)
-            b+= Math.random();
-
-        /*
-         * Select the correct option listed below:
-         *   1. O(N * M) time, O(1) space
-         *   2. O(N + M) time, O(N + M) space
-         *   3. O(N + M) time, O(1) space
-         *   4. O(N * M) time, O(N + M) space
-         *
-         * TODO: return the answer (which option is correct), in the return statement
-        */
-
-        // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
-        return -1;
+        System.out.println("\nTesting complete. Assignment 1 Score is: " + assignmentScore );
     }
-
-
-    public static int algorithmAnalysis2(int n) {
-        int i, j, k = 0;
-        for (i = n/2; i <= n; i++)
-            for ( j = 2; j <= n; j = j*2 )
-                k+= n/2;
-
-        /*
-         * Select the correct option listed below:
-         *   1. O(N) time
-         *   2. O(N log N) time
-         *   3. O(N^2) time
-         *   4. O(N^2Log n) time
-         *
-         * TODO: return the answer (which option is correct), in the return statement
-         */
-
-        // RETURN THE CORRECT OPTION LISTED ABOVE
-        return -1;
-    }
-
 }
 
